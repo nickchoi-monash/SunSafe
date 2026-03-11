cc25155977f378b60ca2cba33c277b50a4d39b5d
nickchoi-monash <nick.choi.it@gmail.com>
2026-03-11 15:50:42 +1100

    initiated the project, removed the original content and css, also prepared the basic page components.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
+import HomePage from './components/pageView/HomePage.vue';
+</script>
+
+<template>
+  <main>
+    <Header />
+    <HomePage />
+    <Footer />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<script></script>
+<template>
+  <header>
+    I am the reusable header
+  </header>
+</template>
