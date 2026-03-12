00c529310bf741b9993e799fa375600f5ef19a37
nickchoi-monash <nick.choi.it@gmail.com>
2026-03-12 16:40:00 +1100

    Installed Bootstrap to the project

--- src/components/pageView/HomePage.vue
+++ src/components/pageView/HomePage.vue
@@ -2,5 +2,9 @@
 <template>
   <header>
     I am the home page view
+
+    Bootstrap test:
+    <button type="button" class="btn btn-primary">Primary</button>
+    <i class="bi bi-apple-music"></i>
   </header>
 </template>
